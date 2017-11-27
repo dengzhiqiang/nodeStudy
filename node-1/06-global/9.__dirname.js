2e826eaa6051c7ea37e249932d0c6de7e8584900
dengzhiqiang <1445124133@qq.com>
2017-11-27 17:57:49 +0800

    'dir_name'

--- node-1/06-global/9.__dirname.js
+++ node-1/06-global/9.__dirname.js
@@ -2,7 +2,7 @@
 console.log(__dirname);
 
 // 目录的绝对路径+文件名名字
-console.log(__filename);c
+console.log(__filename);
 
 // F:\github\node\node-1\06-global
 // F:\github\node\node-1\06-global\9.__dirname.js
