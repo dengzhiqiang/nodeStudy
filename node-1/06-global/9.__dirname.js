a9ca39c12f43e3ca8264a31ade0899413e8e2239
dengzhiqiang <1445124133@qq.com>
2017-11-27 11:18:41 +0800

    '修改cmd'

--- node-1/06-global/9.__dirname.js
+++ node-1/06-global/9.__dirname.js
@@ -2,7 +2,7 @@
 console.log(__dirname);
 
 // 目录的绝对路径+文件名名字
-console.log(__filename);
+console.log(__filename);c
 
 // F:\github\node\node-1\06-global
 // F:\github\node\node-1\06-global\9.__dirname.js
